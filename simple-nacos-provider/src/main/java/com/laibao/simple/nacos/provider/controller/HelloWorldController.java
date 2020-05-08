5640d3dac4bcde8a9b50d4c0c9c8b0a3f0eafab5
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-05-08 08:25:57 +0800

    update pom.xml file info

--- simple-nacos-provider/src/main/java/com/laibao/simple/nacos/provider/controller/HelloWorldController.java
+++ simple-nacos-provider/src/main/java/com/laibao/simple/nacos/provider/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.laibao.simple.nacos.provider;
+package com.laibao.simple.nacos.provider.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
